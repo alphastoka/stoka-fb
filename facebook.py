94791530f469395d1f82ec8192110fb5e8cc4def
= <ssabpisa@me.com>
2016-10-13 20:51:11 +0700

    Can find caption of some initial posts

--- facebook.py
+++ facebook.py
@@ -4,9 +4,34 @@ import json
 import sys, os
 import pika, re
 import zlib
+from bs4 import BeautifulSoup
 
 class FacebookSuggestedPageAPI:
     _cookie = open('cookie.txt', 'r').readline()
+
+    def getPage(self, cookie=_cookie):
+        headers = {
+            "accept-encoding": "utf-8",
+            "accept-language": "en-US,en;q=0.8,th;q=0.6,ja;q=0.4",
+            "upgrade-insecure-requests": "1",
+            "cookie": cookie,
+            "accept": "text/html",
+            "authority": "www.facebook.com",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "cache-control" : "max-age=0",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+        }
+        res = requests.get("https://www.facebook.com/donlaima/", verify=False, headers=headers)
+        res.encoding  = "utf-8"
+        body = res.text
+        soup = BeautifulSoup(body, 'html.parser')
+
+        #posts
+        matches = re.findall(r'<p>([^<]+)</p>', body)
+        for m in matches:
+            print(m)
+        #about
+        
     def querySuggestions(self,cookie=_cookie,pageId='838617286180599'):
         # build request header
         # as realistically as possible to mimic browser
@@ -32,7 +57,8 @@ class FacebookSuggestedPageAPI:
         return A
 
 fsp = FacebookSuggestedPageAPI()
-sugseed = fsp.querySuggestions()
-for sug in sugseed:
-   A = fsp.querySuggestions(pageId=sug[0]) 
-   print(A)
+fsp.getPage()
+# sugseed = fsp.querySuggestions()
+# for sug in sugseed:
+#    A = fsp.querySuggestions(pageId=sug[0]) 
+#    print(A)
